6f04b1527520a8919d459890b15eb7b6fd0e0043
Praveen Jayarajan <praveen.jayarajan@payright.com.au>
2022-06-15 23:02:43 +1000

    Add logs to response

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -4,6 +4,8 @@ const response = (
   statusCode: number,
   body: object
 ): APIGatewayProxyResultV2 => {
+  console.log(statusCode, body);
+
   return {
     statusCode: statusCode || 200,
     body: JSON.stringify(body),
