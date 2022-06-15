e7618065e44363aa6d002a56cd63065209a3eeb0
Praveen Jayarajan <praveen.jayarajan@payright.com.au>
2022-06-15 22:12:12 +1000

    Add console.info

--- src/middleware/checkPermissions.ts
+++ src/middleware/checkPermissions.ts
@@ -3,6 +3,7 @@ import { APIGatewayProxyEventV2 } from "aws-lambda";
 const checkPermissions = (event: APIGatewayProxyEventV2) => {
   try {
     const apiKey = process.env.X_PAYMENTS_API_KEY;
+    console.info(event.headers);
 
     if (event.headers["X-Payments-API-Key"] !== apiKey) {
       return false;
